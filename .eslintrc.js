142536777110754df35d2671cfd3464dc5d4cf2f
davidknutson-mac <davidknutson1@gmail.com>
2022-07-28 21:26:22 -0400

    added eslint, prettier, airbnb formatting to project.

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: 'babel-eslint',
+  plugins: ['react', 'prettier'],
+  extends: ['airbnb', 'prettier'],
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'react/no-unsafe': 'error',
+    'prettier/prettier': ['error'],
+  },
+  env: {
+    es6: true,
+    jest: true,
+    browser: true,
+    node: true,
+  },
+};
